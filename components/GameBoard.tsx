438aeeee91e7a4b1ddcc6f4b1c54bd1cea7b842f
Ovodo <107152769+Ovodo@users.noreply.github.com>
2023-10-30 16:52:43 +0100

    Update GameBoard.tsx

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import React from "react";
 import { Poppins } from "next/font/google";
 import { motion } from "framer-motion";
-import { set } from "mongoose";
 import { useRouter } from "next/router";
 import useSound from "use-sound";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
